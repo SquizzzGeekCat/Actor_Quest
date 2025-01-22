e0c1a0d0f2c6b7f6cbb0c4a33406214752e38b2c
Ludivine CHOMIK <chomik.lu@gmail.com>
2025-01-22 08:57:13 +0100

    probleme with btn erase solve

--- actor_quest/src/js/storage.js
+++ actor_quest/src/js/storage.js
@@ -36,35 +36,18 @@ function getFromLocalstorage() {
   if (actors) {
     const listActors = JSON.parse(actors);
     showHisto(listActors);
-    const btnErraseHisto = elt.createButton(
-      { type: "button", class: "btnErrase" },
-      "Effacer l'historique"
-    );
+    const btnErraseHisto =
+      document.querySelector(".btnErrase") ||
+      elt.createButton(
+        { type: "button", class: "btnErrase" },
+        "Effacer l'historique"
+      );
     btnErraseHisto.addEventListener("click", () => {
       removeFromLocalstorage();
       histo.innerHTML = ""; // Effacer le contenu de l'historique
       btnErraseHisto.remove();
     });
-    // const btnErase = document.querySelector(".btnErrase");
-    // if (!btnErase) {
-    //   const btnErraseHisto = elt.createButton(
-    //     { type: "button", class: "btnErrase" },
-    //     "Effacer l'historique"
-    //   );
-    //   btnErraseHisto.addEventListener("click", () => {
-    //     removeFromLocalstorage();
-    //     histo.innerHTML = ""; // Effacer le contenu de l'historique
-    //     btnErraseHisto.remove();
-    //   });
-    // } else {
-    //   btnErraseHisto.addEventListener("click", () => {
-    //     removeFromLocalstorage();
-    //     histo.innerHTML = ""; // Effacer le contenu de l'historique
-    //     btnErraseHisto.remove();
-    //   });
-    // }
-    //todo: voir si pas plus simple de cree le btn en dur et de le passer a disabled quand pas historique
-    //todo: pour le moment affiche autant de button que de rechagement de localstore
+
     const search = document.getElementById("search");
     search.appendChild(btnErraseHisto);
   } else {
